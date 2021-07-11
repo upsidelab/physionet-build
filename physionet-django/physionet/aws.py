60af0820877e7ad6d2e2ce16d4c2e33e97e44b1c
Chen Xie <is.chenx@gmail.com>
2021-07-10 23:29:20 -0700

    Add s3_ls and s3_create_folder functions

--- physionet-django/physionet/aws.py
+++ physionet-django/physionet/aws.py
@@ -46,14 +46,62 @@ def s3_mv_folder(bucket_name, path1, path2):
     path2 += '/'
 
     s3 = session.resource('s3')
-
     bucket = s3.Bucket(bucket_name)
 
-    for obj in bucket.objects.filter(Prefix=path1):
-        src_key = obj.key
+    for obj_summary in bucket.objects.filter(Prefix=path1):
+        src_key = obj_summary.key
         if src_key.endswith('/'):
             continue
 
         s3.Object(bucket_name, src_key.replace(path1, path2, 1)).copy_from(
             CopySource={ 'Bucket':bucket_name, 'Key': src_key })
-        obj.delete()
+        obj_summary.delete()
+
+
+def s3_ls(bucket_name, folder_path):
+    """
+    Like running 'ls' on an S3 'folder'.
+
+    Note that returned folder_names may differ from what is observed
+    in the AWS console, since only 'folders' that are explicitly
+    created will show up here. ie. Implicit folders from creating
+    an object with key dir1/dir2/filename.txt will not show up.
+
+    """
+    if folder_path.endswith('/'):
+        raise ValueError('folder_path must not end with "/"')
+
+    if folder_path != '':
+        # Ensure this is only checking items within a 'folder'.
+        folder_path += '/'
+    slash_count = folder_path.count('/')
+
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    object_names, folder_names = [], []
+    for obj_summary in bucket.objects.filter(Prefix=folder_path):
+        item_key = obj_summary.key
+
+        if item_key == folder_path:
+            continue
+        if item_key.endswith('/') and item_key.count('/') == slash_count + 1:
+            folder_names.append(item_key)
+        elif item_key.count('/') == slash_count:
+            object_names.append(item_key)
+
+    return object_names, folder_names
+
+
+def s3_create_folder(bucket_name, folder_name, subdir):
+    """
+    Create a 'folder' in an S3 bucket from a subdir.
+    """
+    # Sanitize
+
+
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    bucket.put_object(key=f'{subdir}/{folder_name}/')  # trailing slash very important
+
+
