1744bba3ca714fdb7e1261085cda9ec1e86eac65
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2021-08-05 12:32:00 +0200

    Refactor gcp integration slightly

--- physionet-django/physionet/gcp.py
+++ physionet-django/physionet/gcp.py
@@ -6,16 +6,6 @@ from storages.backends.gcloud import GoogleCloudStorage
 from project.utility import FileInfo, DirectoryInfo, readable_size
 
 
-def create_bucket(bucket_name):
-    client = GoogleCloudStorage().client
-
-    bucket = client.bucket(bucket_name)
-    bucket.location = 'us-west1'
-    bucket.iam_configuration.uniform_bucket_level_access_enabled = True
-
-    client.create_bucket(bucket)
-
-
 class ObjectPath(object):
     def __init__(self, path):
         self._client = None
@@ -28,42 +18,52 @@ class ObjectPath(object):
             raise ValueError('path should specify the bucket and object key/prefix')
 
     def __repr__(self):
-        return f"ObjectPath('{self.bucket_name()}', '{self.key()}')"
+        return f"ObjectPath('{self.bucket_name}', '{self.key}')"
 
+    @property
     def bucket_name(self):
         return self._bucket_name
 
+    @property
     def key(self):
         if self._key == '':
             raise ValueError('object key cannot be empty')
         return self._key
 
+    @property
     def dir_key(self):
         if self._key == '':
             return self._key
         return self._key + '/'
 
+    @property
     def client(self):
         if self._client is None:
             self._client = GoogleCloudStorage().client
         return self._client
 
+    @property
     def bucket(self):
         if self._bucket is None:
-            self._bucket = self.client().get_bucket(self.bucket_name())
+            self._bucket = self.client.get_bucket(self.bucket_name)
         return self._bucket
 
+    @property
     def blob(self):
-        return self.bucket().blob(self.key())
+        return self.bucket.blob(self.key)
 
+    @property
     def dir_blob(self):
-        return self.bucket().blob(self.dir_key())
+        return self.bucket.blob(self.dir_key)
+
+    def batch(self):
+        return self.client.batch()
 
     def create_bucket(self):
-        bucket = self.client().bucket(self.bucket_name())
+        bucket = self.client.bucket(self.bucket_name)
         bucket.location = 'us-west1'
         bucket.iam_configuration.uniform_bucket_level_access_enabled = True
-        self.client().create_bucket(bucket)
+        self.client.create_bucket(bucket)
 
     def create_bucket_if_needed(self):
         try:
@@ -72,42 +72,37 @@ class ObjectPath(object):
             pass
 
     def put(self, data):
-        bucket = self.bucket()
-        blob = bucket.blob(self.key())
+        blob = self.bucket.blob(self.key)
         blob.upload_from_string(data)
 
     def put_fileobj(self, file):
-        bucket = self.bucket()
-        blob = bucket.blob(self.key())
+        blob = self.bucket.blob(self.key)
         blob.upload_from_file(file)
 
     def mkdir(self, **kwargs):
-        bucket = self.bucket()
-        blob = bucket.blob(self.dir_key())
+        blob = self.bucket.blob(self.dir_key)
         blob.upload_from_string('')
 
     def exists(self):
         return self.file_exists() or self.dir_exists()
 
     def file_exists(self):
-        bucket = self.bucket()
-        blob = bucket.blob(self.key())
-        return blob.exists()
+        return self.blob.exists()
 
     def dir_exists(self):
-        iterator = self.client().list_blobs(self.bucket_name(), prefix=self.dir_key(), max_results=1)
+        iterator = self.client.list_blobs(self.bucket_name, prefix=self.dir_key, max_results=1)
         return len(list(iterator)) > 0
 
     def dir_size(self):
-        iterator = self.client().list_blobs(self.bucket_name(), prefix=self.dir_key())
+        iterator = self.client.list_blobs(self.bucket_name, prefix=self.dir_key)
         return sum([obj.size for obj in iterator])
 
     def open(self, mode='rb'):
-        storage = GoogleCloudStorage(bucket_name=self.bucket_name())
-        return storage.open(self.key(), mode=mode)
+        storage = GoogleCloudStorage(bucket_name=self.bucket_name)
+        return storage.open(self.key, mode=mode)
 
     def list_dir(self):
-        iterator = self.client().list_blobs(self.bucket_name(), prefix=self.dir_key(), delimiter='/')
+        iterator = self.client.list_blobs(self.bucket_name, prefix=self.dir_key, delimiter='/')
         blobs = list(iterator)
         prefixes = iterator.prefixes
 
@@ -115,14 +110,14 @@ class ObjectPath(object):
         dirs = []
 
         for blob in blobs:
-            name = blob.name.replace(self.dir_key(), '', 1)
+            name = blob.name.replace(self.dir_key, '', 1)
             if name != '':
                 size = readable_size(blob.size)
                 modified = blob.updated.strftime("%Y-%m-%d")
                 files.append(FileInfo(name, size, modified))
 
         for prefix in prefixes:
-            dirs.append(DirectoryInfo(prefix.replace(self.dir_key(), '', 1)[:-1]))
+            dirs.append(DirectoryInfo(prefix.replace(self.dir_key, '', 1)[:-1]))
 
         files.sort()
         dirs.sort()
@@ -130,8 +125,8 @@ class ObjectPath(object):
         return files, dirs
 
     def url(self):
-        storage = GoogleCloudStorage(bucket_name=self.bucket_name())
-        return storage.url(self.key())
+        storage = GoogleCloudStorage(bucket_name=self.bucket_name)
+        return storage.url(self.key)
 
     def rm(self):
         try:
@@ -142,11 +137,11 @@ class ObjectPath(object):
         self.rm_dir()
 
     def rm_file(self):
-        self.blob().delete()
+        self.blob.delete()
 
     def rm_dir(self):
-        blobs = list(self.client().list_blobs(self.bucket_name(), prefix=self.dir_key()))
-        self.bucket().delete_blobs(blobs=blobs)
+        blobs = list(self.client.list_blobs(self.bucket_name, prefix=self.dir_key))
+        self.bucket.delete_blobs(blobs=blobs)
 
     def cp(self, other):
         try:
@@ -154,17 +149,20 @@ class ObjectPath(object):
         except NotFound:
             pass
 
-        self.cp_directory(other)
+        self.cp_dir(other)
 
     def cp_file(self, other):
-        self.bucket().copy_blob(self.blob(), other.bucket(), new_name=other.key())
+        self.bucket.copy_blob(self.blob, other.bucket, new_name=other.key)
 
     def cp_dir(self, other):
-        iterator = self.client().list_blobs(self.bucket_name(), prefix=self.dir_key())
-        with self.client().batch():
-            for blob in iterator:
-                new_name = blob.name.replace(self.dir_key(), other.dir_key(), 1)
-                self.bucket().copy_blob(blob, other.bucket(), new_name=new_name)
+        iterator = self.client.list_blobs(self.bucket_name, prefix=self.dir_key)
+        try:
+            with self.batch():
+                for blob in iterator:
+                    new_name = blob.name.replace(self.dir_key, other.dir_key, 1)
+                    self.bucket.copy_blob(blob, other.bucket, new_name=new_name)
+        except ValueError: # thrown when there are no batch requests
+            pass
 
     def mv(self, other):
         self.cp(other)
