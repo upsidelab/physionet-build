bd4e957a21335d21aa022ecc15ca731e02d8ef12
Rafał Fiedosiuk <rafal.fiedosiuk@skygate.io>
2022-02-07 16:05:13 +0100

    Add pydantic.

--- physionet-django/environment/views.py
+++ physionet-django/environment/views.py
@@ -1,8 +1,8 @@
-from os import access
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
 
+from environment.services import ResearchEnvironmentService
 from project.models import AccessPolicy, PublishedProject
 
 
@@ -15,3 +15,12 @@ def research_environments(request):
     available_projects = PublishedProject.objects.filter(filters)
 
     return render(request, 'environment/research_environments.html', {'available_projects': available_projects})
+
+
+@login_required
+def set_up_billing_account(request):
+    if request.method == "POST":
+        # if billig account is not set up
+        response = ResearchEnvironmentService.set_up_billing_account(user=request.user)
+
+    return render(request, "")
