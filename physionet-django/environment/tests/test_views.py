d946562419876da9bdcf9449a3da1fec2f620e10
Karol Szuster <karol.szuster@upsidelab.io>
2022-02-16 14:24:06 +0100

    Add boilerplate environment view unittest

--- physionet-django/environment/tests/test_views.py
+++ physionet-django/environment/tests/test_views.py
@@ -1,30 +1,69 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from environment.tests.helpers import create_user_without_cloud_identity, create_user_with_cloud_identity, create_user_with_billing_setup
 
 
 class IdentityProvisioningTestCase(TestCase):
+    url = reverse("identity_provisioning")
+
     def test_redirects_to_billing_setup_if_user_has_cloud_identity(self):
-        pass
+        user = create_user_with_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
 
 class BillingSetupTestCase(TestCase):
+    url = reverse("billing_setup")
+
     def test_redirects_to_identity_provisioning_if_user_has_no_cloud_identity(self):
-        pass
+        user = create_user_without_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
     def test_redirects_to_research_environments_if_user_has_billing_setup(self):
-        pass
+        user = create_user_with_billing_setup()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
 
 class ResearchEnvironmentsTestCase(TestCase):
+    url = reverse("research_environments")
+
     def test_redirects_to_identity_provisioning_if_user_has_no_cloud_identity(self):
-        pass
+        user = create_user_without_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
     def test_redirects_to_billing_setup_if_user_has_no_billing_setup(self):
-        pass
+        user = create_user_with_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
 
 class CreateResearchEnvironmentTestCase(TestCase):
+    url = reverse("create_research_environment")
+
     def test_redirects_to_identity_provisioning_if_user_has_no_cloud_identity(self):
-        pass
+        user = create_user_without_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
 
     def test_redirects_to_billing_setup_if_user_has_no_billing_setup(self):
-        pass
+        user = create_user_with_cloud_identity()
+        self.client.login(username=user.username, password="password")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
