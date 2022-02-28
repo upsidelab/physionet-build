8b0a846c3e42879606a6e628b2c9e031f2d047b6
Karol Szuster <karol.szuster@upsidelab.io>
2022-02-28 16:32:18 +0100

    Cleanup expired environments after data access expired

--- physionet-django/environment/signals.py
+++ physionet-django/environment/signals.py
@@ -1,6 +1,7 @@
 from django.dispatch import receiver
 from django.apps import apps
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.db.models.signals import post_init, post_save
 
 from environment.tasks import stop_environments_with_expired_access
@@ -9,6 +10,8 @@ from environment.tasks import stop_environments_with_expired_access
 User = get_user_model()
 
 Training = apps.get_model("project", "Training")
+
+DataAccessRequest = apps.get_model("project", "DataAccessRequest")
 
 
 @receiver(post_init, sender=User)
@@ -24,11 +27,27 @@ def schedule_stop_environments_if_credentialing_revoked(instance: User, **kwargs
 
 @receiver(post_init, sender=Training)
 def memoize_original_validity(instance: Training, **kwargs):
-    instance._original_validity = instance.is_valid()
+    instance._original_is_valid = instance.is_valid()
 
 
 @receiver(post_init, sender=Training)
-def schedule_environment_(instance: Training, **kwargs):
-    if instance.is_valid() and not instance._original_validity:
+def schedule_stop_environment_if_training_accepted(instance: Training, **kwargs):
+    if instance.is_valid() and not instance._original_is_valid:
         schedule = instance.process_datetime + instance.training_type.valid_duration
+        stop_environments_with_expired_access(instance.id, schedule=schedule)
+
+
+@receiver(post_init, sender=DataAccessRequest)
+def memoize_original_acceptation_status(instance: DataAccessRequest, **kwargs):
+    instance._original_is_accepted = instance.is_accepted()
+
+
+@receiver(post_save, sender=DataAccessRequest)
+def schedule_stop_environment_if_data_access_request_accepted(
+    instance: DataAccessRequest, **kwargs
+):
+    if instance.is_accepted() and not instance._original_is_accepted:
+        if not instance.duration:  # Indefinite access
+            return
+        schedule = timezone.now() + instance.duration
         stop_environments_with_expired_access(instance.id, schedule=schedule)
