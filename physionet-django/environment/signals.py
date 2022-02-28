b8c4390df3542e840f7788eb5a2a695d07350f3a
Karol Szuster <karol.szuster@upsidelab.io>
2022-02-28 16:19:06 +0100

    Cleanup expired environments after training expired

--- physionet-django/environment/signals.py
+++ physionet-django/environment/signals.py
@@ -1,4 +1,5 @@
 from django.dispatch import receiver
+from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_init, post_save
 
@@ -7,6 +8,8 @@ from environment.tasks import stop_environments_with_expired_access
 
 User = get_user_model()
 
+Training = apps.get_model("project", "Training")
+
 
 @receiver(post_init, sender=User)
 def memoize_original_credentialing_status(instance: User, **kwargs):
@@ -14,6 +17,18 @@ def memoize_original_credentialing_status(instance: User, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def schedule_environment_cleanup_background_tasks(instance: User, **kwargs):
+def schedule_stop_environments_if_credentialing_revoked(instance: User, **kwargs):
     if not instance.is_credentialed and instance._original_is_credentialed:
         stop_environments_with_expired_access(instance.id)
+
+
+@receiver(post_init, sender=Training)
+def memoize_original_validity(instance: Training, **kwargs):
+    instance._original_validity = instance.is_valid()
+
+
+@receiver(post_init, sender=Training)
+def schedule_environment_(instance: Training, **kwargs):
+    if instance.is_valid() and not instance._original_validity:
+        schedule = instance.process_datetime + instance.training_type.valid_duration
+        stop_environments_with_expired_access(instance.id, schedule=schedule)
