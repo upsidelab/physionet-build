649c2e91417010b39d6fbab105f128a5c44cea30
Karol Szuster <karol.szuster@upsidelab.io>
2022-03-11 10:39:57 +0100

    Fix given name being swapped with family name in cloud identity

--- physionet-django/environment/api/__init__.py
+++ physionet-django/environment/api/__init__.py
@@ -7,9 +7,9 @@ from environment.api.decorators import api_request
 
 @api_request
 def create_cloud_identity(
-    gcp_user_id: str, family_name: str, given_name: str
+    gcp_user_id: str, given_name: str, family_name: str
 ) -> Request:
-    json = {"userid": gcp_user_id, "familyName": family_name, "givenName": given_name}
+    json = {"userid": gcp_user_id, "givenName": given_name, "familyName": family_name}
     return Request("POST", url="/user", json=json)
 
 
